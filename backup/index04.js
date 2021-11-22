4bdae67c861abc0e49622d60171e6a4845c763e4
Jeremy Ellis <keyfreemusic@gmail.com>
2021-11-22 14:02:32 -0800

    Create index04.js

---
+++ backup/index04.js
@@ -0,0 +1,60 @@
+const http = require("http");
+
+const hostname = "0.0.0.0";
+const port = 8080;
+let oldChunk = "Hello World";
+let myKeys = '';
+
+
+const server = http.createServer((req, res) => {
+  console.log(`\n${req.method} ${req.url}`);
+  console.log(req.headers);
+
+  req.on("data", function(chunk) {
+
+
+
+
+    console.log("BODY: " + chunk);
+    //oldChunk = chunk.toString('ascii');
+    let data = JSON.parse(chunk)
+
+    for (x in data) {   
+        console.log(x +" => "+ data[x]); 
+        myKeys += x + "<br>";
+    }
+    let oldChunk2 = JSON.stringify(data.payload)
+
+    // create a buffer
+    const buff = Buffer.from(oldChunk2, 'base64');
+
+    // decode buffer as UTF-8
+    oldChunk = buff.toString('utf-8');
+
+
+
+
+  });
+  
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  let myPage = `
+  <h2 align=center> this is so cool </h2>
+  <input type=button value=cool onclick="{alert('
+  `+  oldChunk  +`
+  ')}" > <br>
+
+  I really like this<br>
+  
+  ` + myKeys + `
+  
+  <hr> Page end
+  
+  `
+  res.write(myPage);
+  res.end();
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
