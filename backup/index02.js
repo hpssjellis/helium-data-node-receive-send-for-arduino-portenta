1bdabdcd6c38dd05b120d65ce1e27d88adf23aa3
Jeremy Ellis <keyfreemusic@gmail.com>
2021-11-22 13:27:45 -0800

    Create index02.js

---
+++ backup/index02.js
@@ -0,0 +1,44 @@
+// reminders:
+// Make your gitpod public and open the browser
+// on Helium integegrations set the endpoint to the exact URL of the browser 
+
+
+
+
+const http = require("http");
+
+const hostname = "0.0.0.0";
+const port = 8080;
+var oldChunk = "Hello World";
+
+const server = http.createServer((req, res) => {
+  console.log(`\n${req.method} ${req.url}`);
+  console.log(req.headers);
+
+  req.on("data", function(chunk) {
+    console.log("BODY: " + chunk);
+    //oldChunk = chunk.toString('ascii');
+    let data = JSON.parse(chunk)
+    let oldChunk2 = JSON.stringify(data.payload)
+
+    // create a buffer
+    const buff = Buffer.from(oldChunk2, 'base64');
+
+    // decode buffer as UTF-8
+    oldChunk = buff.toString('utf-8');
+
+
+
+
+  });
+  
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  let myPage = "<input type=button value=cool onclick=\"{alert(\'wow\')}\" > "
+  res.write(myPage);
+  res.end(oldChunk);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
