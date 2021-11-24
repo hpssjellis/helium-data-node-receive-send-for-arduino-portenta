41ea9f5d92be4689d9586ee579a5542bb25ef746
Jeremy Ellis <keyfreemusic@gmail.com>
2021-11-24 09:03:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,52 @@ const http = require("http");
 
 const hostname = "0.0.0.0";
 const port = 8080;
-var oldChunk = "Hello World";
+let oldChunk = "Hello World";
+let myKeys = '';
+let myHotspots = '';
+let myMetaData = '';
+let myLabels = '';
+let myAllKeys = '';
 
 const server = http.createServer((req, res) => {
   console.log(`\n${req.method} ${req.url}`);
   console.log(req.headers);
 
   req.on("data", function(chunk) {
+
+
+
+
     console.log("BODY: " + chunk);
     //oldChunk = chunk.toString('ascii');
     let data = JSON.parse(chunk)
+
+    for (x in data) {   
+      //  console.log(x +" => "+ data[x]); 
+        myKeys += x + "<br>";
+    }
+    for (let y=0; y < data.hotspots.length; y++){
+      for (x in data.hotspots[y]) {   
+         // console.log(x +" => "+ data.hotspots[x]); 
+          myHotspots += x + "<br>";
+      }
+    }
+    /*
+    Object.keys(data).forEach(key => { 
+       // console.log(jsonObject[key]); //values 
+        console.log(key); //keys 
+        myAllKeys += key + "<br>";
+    })
+    */
+
     let oldChunk2 = JSON.stringify(data.payload)
 
     // create a buffer
     const buff = Buffer.from(oldChunk2, 'base64');
 
     // decode buffer as UTF-8
-    oldChunk = buff.toString('utf-8');
+    oldChunk = buff.toString('utf-8');  //.trim();  // also removes spaces
+  
 
 
 
@@ -28,9 +57,24 @@ const server = http.createServer((req, res) => {
   
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  let myPage = "<input type=button value=cool onclick=\"{alert(\'wow\')}\" > "
+  let myPage = `
+  <h2 align=center> this is so cool </h2>
+  <input type=button value=payload onclick="{alert('`+  oldChunk  +`')}" > <br>
+
+
+
+  Just showing the keys on the webpage not the values<br>
+  
+  ` + myKeys + `
+  <h3>in the hotspots key:</h3>
+  ` + myHotspots + `
+
+  
+  <hr> Page end
+  
+  `
   res.write(myPage);
-  res.end(oldChunk);
+  res.end();
 });
 
 server.listen(port, hostname, () => {
