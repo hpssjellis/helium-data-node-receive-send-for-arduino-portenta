5c777fbb01b14931a032f89f6db3ed6ba88ead89
Jeremy Ellis <keyfreemusic@gmail.com>
2021-11-30 20:24:47 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-
 const http = require("http");
 
 const hostname = "0.0.0.0";
 const port = 8080;
-let myNoShow = [];   // Key for which we should not show the value when taking a picature of the screen
+let myNoShow = [];   // key to values to hide!
 //let myNoShow = ["app_eui","dev_eui","lat","long", "name","devaddr","downlink_url","id","organization_id","uuid"];
-let oldChunk = "Hello World";
-let myDataType='';
-let myKeys = '';
-let myHotspots = '';
-let myMetaData = '';
-let myLabels = '';
-let myAllKeys = '';
+
 let myHTML = 'Hello World';
-
-
-
 
 
 function myJsonToHtml( json, myNoNoShow, myHideTrueFalse ) {
   let myOutput = "<ul>";
   for (const property in json) {
     if( typeof json[property] === 'string' || typeof json[property] === 'number' || typeof json[property] === 'boolean' || typeof json[property] === 'bigint') {
-
         if (myNoNoShow.includes(property)) {
           myOutput += "<li>" + property + " : *******************"; 
         } else {
@@ -34,11 +23,9 @@ function myJsonToHtml( json, myNoNoShow, myHideTrueFalse ) {
    
     } else if( Array.isArray( json[property] ) ) {
        myOutput += "<br><ul>" + property;
-    	  for( let i=0; i < json[property].length; i++ ) {
-            // myOutput += "<li>" + property + "[" + i + "]";          
+    	  for( let i=0; i < json[property].length; i++ ) {      
              myOutput +=  property + "[" + i + "]";         
-             myOutput +=  myJsonToHtml(json[property][i], myNoNoShow)  // recursive call this
-             
+             myOutput +=  myJsonToHtml(json[property][i], myNoNoShow)  // recursive call this        
        }
        myOutput += "</ul>";
     }
@@ -52,81 +39,17 @@ const server = http.createServer((req, res) => {
   console.log(req.headers);
 
   req.on("data", function(chunk) {
-    oldChunk = "Hello World";
-    myDataType='';
-    myKeys = '';
-    myHotspots = '';
-    myMetaData = '';
-    myLabels = '';
-    myAllKeys = '';
-
-
 
     console.log("BODY: " + chunk);   // Logs everything coming in
 
-
     let data = JSON.parse(chunk)
     myHTML = myJsonToHtml(data, myNoShow)
-    for (x in data) {   
-      //  console.log(x +" => "+ data[x]); 
-        myKeys += x + "<br>";
-    }
-    for (let y=0; y < data.hotspots.length; y++){
-      myHotspots += "<br><b>Hotspot Array</b> #" + y + "<br>";
-      for (x in data.hotspots[y]) {   
-         // console.log(x +" => "+ data.hotspots[x]); 
-  
-          myHotspots += x + "<br>";
-      }
-    }
-    /*
-    Object.keys(data).forEach(key => { 
-       // console.log(jsonObject[key]); //values 
-        console.log(key); //keys 
-        myAllKeys += key + "<br>";
-    })
-    */
-   let oldChunk2, buff;
-
-   myDataType = data.type
-    if (data.type == "uplink"){
-    oldChunk2 = JSON.stringify(data.payload)
-    buff = Buffer.from(oldChunk2, 'base64');
-    } else {
-      buff = "Not Uplink!"
-    }
-    // create a buffer
-
-
-    // decode buffer as UTF-8
-    oldChunk = buff.toString('utf-8');  //.trim();  // also removes spaces
-  
-
-
-
 
   });
   
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  let myPage = `
-  <h2 align=center> this is so cool </h2>
-  <input type=button value=payload onclick="{alert('`+  oldChunk  +`')}" > <br>
 
-
-
-   Just showing the keys on the webpage not the values<br>
-   Note that this is a: join/uplink:<h3> 
-   `+ myDataType + ` </h3>
-  ` + myKeys + `
-  <h3>in the hotspots key:</h3>
-  ` + myHotspots + `
-
-  
-  <hr> Page end
-  
-  `
-  //res.write(myPage);
   res.write(myHTML);
   res.end();
 
